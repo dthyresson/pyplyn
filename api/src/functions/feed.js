8b2957d8b3e139d449514981a78f2dd680bea4da
A. David Thyresson <dthyresson@gmail.com>
2020-09-23 21:43:11 -0400

    Change feed function to be a post w/ body params

--- api/src/functions/feed.js
+++ api/src/functions/feed.js
@@ -1,16 +1,33 @@
+import { logger } from 'src/lib/logger/logger'
 import { streamContents } from 'src/lib/apiClients/feedly'
 import { persistTweets } from 'src/services/tweetJobs'
 
 export const handler = async (event, _context) => {
-  const { streamId, count, continuation } = event.queryStringParameters
+  const { streamId, count, continuation } = JSON.parse(event.body)
+  logger.info(streamId)
+
   const data = await streamContents({ streamId, count, continuation })
+  logger.info({ streamId, count, continuation })
+  try {
+    await persistTweets({ response: data })
 
-  await persistTweets({ response: data })
+    const { id, updated, continuation } = data
+    const itemsCount = data?.items?.length || 0
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data,
-    }),
+    logger.info({ streamId, count, continuation, itemsCount })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: { id, updated, continuation, itemsCount },
+      }),
+    }
+  } catch (e) {
+    logger.error(e)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: e,
+      }),
+    }
   }
 }
