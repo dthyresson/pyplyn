3be1579d9f03f301b1f1580586b9f39b1c07fdab
A. David Thyresson <dthyresson@gmail.com>
2020-10-02 12:09:15 -0400

    Line tooltip

--- web/src/components/LineChartCell/LineChartCell.js
+++ web/src/components/LineChartCell/LineChartCell.js
@@ -46,14 +46,39 @@ const MyResponsiveLine = ({ data /* see data tab */ }) => (
       legendOffset: -40,
       legendPosition: 'middle',
     }}
-    colors={{ scheme: 'purple_blue' }}
+    colors={{ scheme: 'paired' }}
     curve={'natural'}
     lineWidth={2}
     pointSize={4}
     pointColor={{ theme: 'background' }}
     pointBorderWidth={2}
     pointBorderColor={{ from: 'serieColor' }}
-    pointLabel="y"
+    tooltip={({ point }) => {
+      return (
+        <div className="col-span-1 flex shadow-sm rounded-md">
+          <div
+            style={{ backgroundColor: point.serieColor }}
+            className="flex-shrink-0 flex items-center justify-center w-16 text-white text-sm leading-5 font-medium rounded-l-md"
+          >
+            {point.data.yFormatted}
+          </div>
+          <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+            <div className="flex-1 px-4 py-2 text-sm leading-5 truncate">
+              <a
+                href="#"
+                className="text-gray-900 font-medium hover:text-gray-600 transition ease-in-out duration-150"
+              >
+                {point.serieId}
+              </a>
+              <p className="text-gray-500">
+                {' '}
+                Mentions on {point.data.xFormatted}
+              </p>
+            </div>
+          </div>
+        </div>
+      )
+    }}
     pointLabelYOffset={-12}
     useMesh={true}
     legends={[
@@ -82,7 +107,7 @@ const MyResponsiveLine = ({ data /* see data tab */ }) => (
         ],
       },
     ]}
-    enableSlices="x"
+    enableSlices={false}
     sliceTooltip={({ slice }) => {
       return (
         <div
