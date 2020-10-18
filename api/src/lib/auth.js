685dbe3106dfa0c79bfdf0137b58b8aac2680494
A. David Thyresson <dthyresson@gmail.com>
2020-10-18 19:47:55 -0400

    log auth

--- api/src/lib/auth.js
+++ api/src/lib/auth.js
@@ -1,59 +1,4 @@
-// Define what you want `currentUser` to return throughout your app. For example,
-// to return a real user from your database, you could do something like:
-//
-//   export const getCurrentUser = async ({ email }) => {
-//     return await db.user.findOne({ where: { email } })
-//   }
-//
-// If you want to enforce role-based access ...
-//
-// You'll need to set the currentUser's roles attributes to the
-// collection of roles as defined by your app.
-//
-// This allows requireAuth() on the api side and hasRole() in the useAuth() hook on the web side
-// to check if the user is assigned a given role or not.
-//
-// How you set the currentUser's roles depends on your auth provider and its implementation.
-//
-// For example, your decoded JWT may store `roles` in it namespaced `app_metadata`:
-//
-// {
-//   'https://example.com/app_metadata': { authorization: { roles: ['admin'] } },
-//   'https://example.com/user_metadata': {},
-//   iss: 'https://app.us.auth0.com/',
-//   sub: 'email|1234',
-//   aud: [
-//     'https://example.com',
-//     'https://app.us.auth0.com/userinfo'
-//   ],
-//   iat: 1596481520,
-//   exp: 1596567920,
-//   azp: '1l0w6JXXXXL880T',
-//   scope: 'openid profile email'
-// }
-//
-// The parseJWT utility will extract the roles from decoded token.
-//
-// The app_medata claim may or may not be namespaced based on the auth provider.
-// Note: Auth0 requires namespacing custom JWT claims
-//
-// Some providers, such as with Auth0, will set roles an authorization
-// attribute in app_metadata (namespaced or not):
-//
-// 'app_metadata': { authorization: { roles: ['publisher'] } }
-// 'https://example.com/app_metadata': { authorization: { roles: ['publisher'] } }
-//
-// Other providers may include roles simply within app_metadata:
-//
-// 'app_metadata': { roles: ['author'] }
-// 'https://example.com/app_metadata': { roles: ['author'] }
-//
-// And yet other may define roles as a custom claim at the root of the decoded token:
-//
-// roles: ['admin']
-//
-// The function `getCurrentUser` should return the user information
-// together with a collection of roles to check for role assignment:
+import { logger } from 'src/lib/logger'
 
 import { AuthenticationError, ForbiddenError, parseJWT } from '@redwoodjs/api'
 
@@ -97,7 +42,8 @@ import { AuthenticationError, ForbiddenError, parseJWT } from '@redwoodjs/api'
  *   }
  * }
  */
-export const getCurrentUser = async (decoded, { _token, _type }) => {
+export const getCurrentUser = async (decoded, { token, type }) => {
+  logger.debug({ decoded, token, type }, 'in getCurrentUser')
   return { ...decoded, roles: parseJWT({ decoded }).roles }
 }
 
@@ -122,7 +68,10 @@ export const getCurrentUser = async (decoded, { _token, _type }) => {
  * requireAuth({ role: ['publisher'] })
  */
 export const requireAuth = ({ role } = {}) => {
+  logger.debug({ role }, 'in requireAuth')
+
   if (!context.currentUser) {
+    logger.error("You don't have permission to do that.")
     throw new AuthenticationError("You don't have permission to do that.")
   }
 
@@ -131,6 +80,10 @@ export const requireAuth = ({ role } = {}) => {
     typeof role === 'string' &&
     !context.currentUser.roles?.includes(role)
   ) {
+    logger.error(
+      { currentUser: context.currentUser, role },
+      "You don't have access to do that."
+    )
     throw new ForbiddenError("You don't have access to do that.")
   }
 
@@ -139,6 +92,10 @@ export const requireAuth = ({ role } = {}) => {
     Array.isArray(role) &&
     !context.currentUser.roles?.some((r) => role.includes(r))
   ) {
+    logger.error(
+      { currentUser: context.currentUser, role },
+      "You don't have access to do that."
+    )
     throw new ForbiddenError("You don't have access to do that.")
   }
 }
