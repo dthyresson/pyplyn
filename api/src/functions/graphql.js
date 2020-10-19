94135ca92f2ebfd5f78995f75581872f250b3264
A. David Thyresson <dthyresson@gmail.com>
2020-10-19 02:59:03 -0400

    GraphQL needed to allow the redwood in shield permissions

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -9,7 +9,7 @@ import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth'
 
-// import { applyMiddleware } from 'graphql-middleware'
+import { applyMiddleware } from 'graphql-middleware'
 import { allow, deny, _rule, shield } from 'graphql-shield'
 
 import { logger } from 'src/lib/logger'
@@ -21,73 +21,74 @@ import { db } from 'src/lib/db'
 //   }
 // )
 
-// const queryPermissions = {
-//   '*': allow,
-//   articleById: allow,
-//   articles: allow,
-//   articlesForLabel: allow,
-//   paginateArticles: allow,
-//   tagSummaries: allow,
-//   tagSummariesForLabel: allow,
-//   paginateTagSummaries: allow,
-//   tweetById: allow,
-//   tweets: allow,
-//   tweetStats: allow,
-//   tweetsForLabel: allow,
-//   paginateTweets: allow,
-//   bumpChart: allow,
-//   lineChart: allow,
-//   repeaterJobs: allow,
-//   repeaterJob: allow,
-//   repeaterJobResults: allow,
-//   repeaterJobChart: allow,
-//   periodTotalStats: allow,
-//   popularTags: allow,
-//   lastDurationPeriodTotalStats: allow,
-// }
+const queryPermissions = {
+  '*': allow,
+  redwood: allow,
+  articleById: allow,
+  articles: allow,
+  articlesForLabel: allow,
+  paginateArticles: allow,
+  tagSummaries: allow,
+  tagSummariesForLabel: allow,
+  paginateTagSummaries: allow,
+  tweetById: allow,
+  tweets: allow,
+  tweetStats: allow,
+  tweetsForLabel: allow,
+  paginateTweets: allow,
+  bumpChart: allow,
+  lineChart: allow,
+  repeaterJobs: allow,
+  repeaterJob: allow,
+  repeaterJobResults: allow,
+  repeaterJobChart: allow,
+  periodTotalStats: allow,
+  popularTags: allow,
+  lastDurationPeriodTotalStats: allow,
+}
 
-// const permissions = shield(
-//   {
-//     Query: queryPermissions,
-//     Mutation: {
-//       '*': deny,
-//     },
-//   },
-//   {
-//     fallbackRule: allow, // this allows the nested types in a query to be resolved
-//     fallbackError: (thrownThing, _parent, args, _context, info) => {
-//       if (thrownThing instanceof ApolloError) {
-//         // expected errors
-//         logger.error({ args, thrownThing }, 'Not permitted')
-//         return new Error('Not permitted')
-//       } else if (thrownThing instanceof Error) {
-//         // unexpected errors
-//         logger.error({ args, thrownThing }, 'The resolver threw an error.')
-//         return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER')
-//       } else {
-//         logger.error(
-//           { args, thrownThing, info },
-//           'The resolver threw something that is not an error.'
-//         )
-//         return new Error('Not permitted')
-//       }
-//     },
-//   }
-// )
+const permissions = shield(
+  {
+    Query: queryPermissions,
+    Mutation: {
+      '*': deny,
+    },
+  },
+  {
+    fallbackRule: allow, // this allows the nested types in a query to be resolved
+    fallbackError: (thrownThing, _parent, args, _context, info) => {
+      if (thrownThing instanceof ApolloError) {
+        // expected errors
+        logger.error({ args, thrownThing }, 'Not permitted')
+        return new Error('Not permitted')
+      } else if (thrownThing instanceof Error) {
+        // unexpected errors
+        logger.error({ args, thrownThing }, 'The resolver threw an error.')
+        return new ApolloError('Internal server error', 'ERR_INTERNAL_SERVER')
+      } else {
+        logger.error(
+          { args, thrownThing, info },
+          'The resolver threw something that is not an error.'
+        )
+        return new Error('Not permitted')
+      }
+    },
+  }
+)
 
 export const handler = createGraphQLHandler({
   getCurrentUser,
-  // schema: applyMiddleware(
-  //   makeMergedSchema({
-  //     schemas,
-  //     services: makeServices({ services }),
-  //   }),
-  //   permissions
-  // ),
-  schema: makeMergedSchema({
-    schemas,
-    services: makeServices({ services }),
-  }),
+  schema: applyMiddleware(
+    makeMergedSchema({
+      schemas,
+      services: makeServices({ services }),
+    }),
+    permissions
+  ),
+  // schema: makeMergedSchema({
+  //   schemas,
+  //   services: makeServices({ services }),
+  // }),
   cors: {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
